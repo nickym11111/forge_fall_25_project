485744b0be7c881aac43c7b57cefd40341fd764a
Ashwin Iyer <iyer.ashw@northeastern.edu>
2025-10-04 13:17:54 -0400

    add Supabase configuration to environment and update requirements

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,21 @@ from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
 
 
 app = FastAPI()
+
+
 
 origins = [
     "http://localhost:8081", # React/Next dev server
