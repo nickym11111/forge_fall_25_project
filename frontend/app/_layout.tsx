7c66cf87cc2ced1e8ab3039124f45b8ff9b8446e
Nicky Mosharaf <nickymosharaf@Nickys-MacBook-Air.local>
2025-10-08 13:32:53 -0400

    removed headers from tabs

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -50,8 +50,8 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
---
+++ frontend/app/account/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+
+export default function AccountLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+    </Stack>
+  );
+}
