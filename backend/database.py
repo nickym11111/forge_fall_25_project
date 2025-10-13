2cd85aa70732e3487041820e94a4061999b39f01
stevenn-d <dong.stev@northeastern.edu>
2025-10-13 17:26:22 -0400

    update backend

--- backend/database.py
+++ backend/database.py
@@ -5,15 +5,10 @@ import os
 from dotenv import load_dotenv # type: ignore
 from supabase import create_client
 
+
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-print(f"DEBUG: Connecting with URL: '{url}'")
-
-if not url or not key:
-    raise ValueError("ERROR: SUPABASE_URL or SUPABASE_KEY is missing. Check your .env file.")
-
-supabase = create_client(url, key)
-
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
