0e7092cb20f6d26967eae909ac5449a3306e8636
Ashwin Iyer <iyer.ashw@northeastern.edu>
2025-10-14 20:02:22 -0400

    Refactor code structure for improved readability and maintainability

---
+++ backend/receipt-parsing/chatGPTParse.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv # type: ignore
+import base64
+import os
+from openai import OpenAI
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+    
+image_path = "./testReceipt.jpg"
+
+
+base64_image = encode_image(image_path)
+
+response = client.responses.create(
+    model="gpt-4.1",
+    input=[
+        {
+            "role": "user",
+            "content": [
+                { "type": "input_text", "text": "parse this receipt for text?" },
+                {
+                    "type": "input_image",
+                    "image_url": f"data:image/jpeg;base64,{base64_image}",
+                },
+            ],
+        }
+    ],
+)
+
+print(response.output_text)
