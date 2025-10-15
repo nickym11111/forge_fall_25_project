343df45b91c9766be42b9e83945e715e568535a5
Ashwin Iyer <iyer.ashw@northeastern.edu>
2025-10-15 18:59:35 -0400

    Add receipt parsing endpoint and update API routes

--- backend/receiptParsing/chatGPTParse.py
+++ backend/receiptParsing/chatGPTParse.py
@@ -1,10 +1,17 @@
 from dotenv import load_dotenv # type: ignore
-import base64
 import os
 from openai import OpenAI
+from pydantic import BaseModel
+from fastapi import HTTPException, APIRouter, FastAPI
+
+
 load_dotenv()
+app = APIRouter()
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
+
+class Receipt(BaseModel):
+    base64Image: str
 
 
 def getChatGPTResponse(base64_image: str):
@@ -25,3 +32,12 @@ def getChatGPTResponse(base64_image: str):
     )
 
     return response
+
+@app.post("/parse-receipt")
+def parse_receipt(receipt: Receipt):
+    try:
+        return getChatGPTResponse(receipt.base64Image);
+    except Exception as e:
+        error_msg = f"Error parsing receipt: {str(e)}"
+        print(error_msg)
+        raise HTTPException(status_code=500, detail=error_msg)
