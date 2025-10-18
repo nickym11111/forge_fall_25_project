88d63936bdd4a71bf172089183aa79ecba547ae6
Hannah Bang <bang.ha@northeastern.edu>
2025-10-17 20:24:37 -0400

    first

---
+++ backend/recipes.py
@@ -0,0 +1,21 @@
+import ollama
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import uvicorn # Needed to run the server
+
+app = FastAPI()
+
+@app.post("/find_ingredients")
+async def find_ingredients(recipe):
+    result = ollama.generate(
+        model='llama3',
+        prompt=f'''
+        What ingredients do I need to make {recipe}? Return this as a list of items.
+        '''
+    )
+    
+    return {"message": result['response']}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
