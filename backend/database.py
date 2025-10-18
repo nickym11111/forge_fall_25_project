b7fd762ff15e066e4bde2de65cf9964514b15bc3
stevenn-d <dong.stev@northeastern.edu>
2025-10-18 01:48:01 -0400

    send fridge invite completed

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv # type: ignore
+from supabase import create_client
+
 
 load_dotenv()
 
@@ -14,4 +16,5 @@ print(f"DEBUG: Connecting with URL: '{url}'")
 if not url or not key:
     raise ValueError("ERROR: SUPABASE_URL or SUPABASE_KEY is missing. Check your .env file.")
 
-supabase: Client = create_client(url, key)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
