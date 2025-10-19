cea91cb94978e1e7798286dab5e69e9d345329c1
stevenn-d <dong.stev@northeastern.edu>
2025-10-19 19:19:04 -0400

    Revert "10/18"
    This reverts commit 96318e66301a182a4eb987c4b5d0a6b189acf701.

--- backend/recipes.py
+++ backend/recipes.py
@@ -21,9 +21,8 @@ def getChatGPTResponse(recipe: str):
                 "role": "user",
                 "content": [
                     { "type": "input_text", "text": f'''
-                        What ingredients do I need to make {recipe}? Return this as a JSON object with no
-                        word response or ```''' 
-                    },
+        What ingredients do I need to make {recipe}? Return this as a list of items.
+        ''' },
                 ],
             }
         ],
