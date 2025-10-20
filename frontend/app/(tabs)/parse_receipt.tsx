27fd4824893d35a813c78dffa362cb9b12474a81
Ashwin Iyer <iyer.ashw@northeastern.edu>
2025-10-20 19:36:50 -0400

    fix images on mobile

--- frontend/app/(tabs)/parse_receipt.tsx
+++ frontend/app/(tabs)/parse_receipt.tsx
@@ -9,7 +9,7 @@ import {
   Alert,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import * as FileSystem from "expo-file-system";
+    import { File } from 'expo-file-system';
 import { CreateParseReceiptRequest } from "../api/ParseReceipt";
 import CustomHeader from "@/components/CustomHeader";
 import { TouchableOpacity } from "react-native";
@@ -139,11 +139,9 @@ export default function ParseReceiptScreen() {
           reader.readAsDataURL(blob);
         });
       } else {
-        base64Image = await FileSystem.readAsStringAsync(imageUri, {
-          encoding: "base64",
-        });
+        const file = new File(imageUri);
+        base64Image = file.base64Sync();
       }
-
       const response = await CreateParseReceiptRequest(base64Image);
       const parsed = JSON.parse(response.output[0].content[0].text);
       setResponseText(JSON.stringify(parsed, null, 2));
