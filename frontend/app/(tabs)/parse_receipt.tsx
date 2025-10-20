4048e8e7013d1c2e8d4d2702a7193447dd68523d
Ashwin Iyer <iyer.ashw@northeastern.edu>
2025-10-20 19:43:27 -0400

    refactor: replace View with ScrollView for improved layout handling

--- frontend/app/(tabs)/parse_receipt.tsx
+++ frontend/app/(tabs)/parse_receipt.tsx
@@ -7,6 +7,7 @@ import {
   Platform,
   StyleSheet,
   Alert,
+  ScrollView
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
     import { File } from 'expo-file-system';
@@ -152,7 +153,7 @@ export default function ParseReceiptScreen() {
   };
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <CustomHeader title="Add Items 📷" />
       <View style={{ position: "fixed", zIndex: 999, left: 0, right: 20 }}>
         <ToastMessage message={toastMessage} visible={isToastVisible} />
@@ -179,7 +180,7 @@ export default function ParseReceiptScreen() {
         {responseText}
       </Text>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
