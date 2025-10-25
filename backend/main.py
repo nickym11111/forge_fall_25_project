154bc7c540a0da8169b17339009f725067cb3bb4
Varun <vmeka8@gmail.com>
2025-10-25 14:51:07 -0400

    Changed import.

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ load_dotenv()
 # Initialize routers
 app = FastAPI()
 app.include_router(users_router)
-app.include_router(ai_expiration, tags=["ai"])
+#app.include_router(ai_expiration, tags=["ai"])
 
 # Allow CORS origin policy to allow requests from local origins.
 origins = [
