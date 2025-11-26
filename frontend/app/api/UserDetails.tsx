0bb50c3044d8f9948f1015257dddfd449cc5b2c0
Nicky Mosharaf <nickymosharaf@Nickys-MacBook-Air.local>
2025-11-26 23:06:17 +0100

    fixed my user detail merge mistake

--- frontend/app/api/UserDetails.tsx
+++ frontend/app/api/UserDetails.tsx
@@ -84,27 +84,3 @@ export async function leaveFridge(fridgeId: string) {
         return null;
       }
 }
-
-export async function leaveFridge(fridgeId: string) {
-      try {
-        const { data: { session } } = await supabase.auth.getSession();
-        if (!session) {
-          return null;
-        }
-        const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/fridge/leave-fridge`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${session.access_token}`
-          },
-          body: JSON.stringify({ fridgeId, userId: session.user.id })
-        });
-        
-        if (response.ok) {
-          return(await response.json());
-        }
-      } catch (error) {
-        console.error('Error fetching user:', error);
-        return null;
-      }
-}
