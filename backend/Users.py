331b9ab6b3487f3c8a67abaa81d6b3ca240c7a1c
Ashwin Iyer <iyer.ashw@northeastern.edu>
2025-12-01 18:25:16 -0500

    uhh idk:

--- backend/Users.py
+++ backend/Users.py
@@ -56,8 +56,8 @@ async def create_user(user: UserCreate):
     except Exception as e:
         return {"error": str(e)}
 
-@app.get("/userInfo")
-async def get_current_user_info(current_user = Depends(get_current_user_with_fridgeMates)):
+@app.get("/userInfo/")
+async def get_current_user_info(current_user = Depends(get_current_user)):
     try:
         user_data = current_user if isinstance(current_user, dict) else {
             "id": current_user.id,
